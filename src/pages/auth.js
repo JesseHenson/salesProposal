4cd89670502a2ee602e610f516f007931a5522eb
Jesse Henson <jesse.henson@staffscapes.com>
2020-03-03 14:37:35 -0700

    commit 3

---
+++ src/pages/auth.js
@@ -0,0 +1,44 @@
+import React from "react"
+import IdentityModal, {
+  useIdentityContext,
+  IdentityContextProvider,
+} from "react-netlify-identity-widget"
+import "react-netlify-identity-widget/styles.css"
+
+function App() {
+  const url = "https://sales-proposal-staffscapes.netlify.com/" // supply the url of your Netlify site instance. VERY IMPORTANT. no point putting in env var since this is public anyway
+  return (
+    <IdentityContextProvider url={url}>
+      <AuthStatusView />
+    </IdentityContextProvider>
+  )
+}
+export default App
+
+function AuthStatusView() {
+  const identity = useIdentityContext()
+  const [dialog, setDialog] = React.useState(false)
+  const name =
+    (identity &&
+      identity.user &&
+      identity.user.user_metadata &&
+      identity.user.user_metadata.name) ||
+    "NoName"
+  const isLoggedIn = identity && identity.isLoggedIn
+  return (
+    <div>
+      <div>
+        <button className="RNIW_btn" onClick={() => setDialog(true)}>
+          {isLoggedIn ? `Hello ${name}, Log out here!` : "Log In"}
+        </button>
+      </div>
+      <IdentityModal
+        showDialog={dialog}
+        onCloseDialog={() => setDialog(false)}
+        onLogin={user => console.log("hello ", user.user_metadata)}
+        onSignup={user => console.log("welcome ", user.user_metadata)}
+        onLogout={() => console.log("bye ", name)}
+      />
+    </div>
+  )
+}
